ee88a80afd1e0809b2c9155e953e34ebee254fd9
ando07 <ando.masahito@gmail.com>
2019-04-29 21:20:39 +0900

    [clean]リファクタリング

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,22 +1,27 @@
 <template>
-    <v-btn @click="onClick">MyButton</v-btn>
+  <v-btn @click="onClick">MyButton</v-btn>
 </template>
 
 <script lang="ts">
-import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+  import {
+    Component,
+    Emit,
+    Prop,
+    Vue,
+  } from 'vue-property-decorator';
 
-    @Component
-export default class MyButton extends Vue {
-        @Prop()
-        public greet?: string;
+  @Component
+  export default class MyButton extends Vue {
+    @Prop()
+    public greet ? : string;
 
-        @Emit()
-        public clicked() {
-        }
+    @Emit()
+    public clicked() {}
 
-        public onClick() {
-          alert(this.greet);
-          this.clicked();
-        }
-}
+    public onClick() {
+      alert(this.greet);
+      this.clicked();
+    }
+  }
+
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,23 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import MyButton from '@/components/MyButton.vue';
+  import {
+    Component,
+    Vue
+  } from 'vue-property-decorator';
+  import MyButton from '@/components/MyButton.vue';
 
   @Component({
     components: {
       MyButton,
     },
   })
-export default class Home extends Vue {
+  
+  export default class Home extends Vue {
     public greetText: string = 'Hello';
 
     public onMyButtonClicked() {
-      this.greetText = "こんにちは";
+      this.greetText = 'こんにちは';
     }
-}
+  }
 </script>
